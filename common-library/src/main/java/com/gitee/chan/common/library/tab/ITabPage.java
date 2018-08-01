e1507f209ae1f6538955a3c95bb89ae17075741f
johnny <chmj1208@126.com>
2018-08-02 00:25:04 +0800

    serviceLoad 示例

---
+++ common-library/src/main/java/com/gitee/chan/common/library/tab/ITabPage.java
@@ -0,0 +1,25 @@
+package com.gitee.chan.common.library.tab;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+
+/**
+ * ================================================
+ * Description:  Tab配置页注解
+ *
+ * Created by JohnnyChan on 2018/8/1
+ *
+ * ================================================
+ */
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
+public @interface ITabPage {
+
+    String tabName();
+
+    String iconName();
+}
